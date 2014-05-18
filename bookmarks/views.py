f61b657ee4c37675c665ed7f8dfd6cbbb0cf8f92
nhamlv-55 <nhamlevan@gmail.com>
2014-05-18 23:58:01 +0900

    second commit

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,23 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from django.template import Context
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import Context, RequestContext
 from django.template.loader import get_template
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render_to_response, render, get_object_or_404
+from bookmarks.forms import *
 
+# def main_page(request):
+# 	# to load the main_page.html template
+# 	template = get_template('main_page.html')
+# 	# Set the variable in the template
+# 	variables = Context({
+# 		'head_title': 'Main page',
+# 		'page_title': 'Welcome to Elmusico',
+# 		'page_body': 'Blah blah blah blah blah'
+# 		})
+# 	#pass the variables and create a HTML output
+# 	output = template.render(variables)
+# 	#return it
+# 	return HttpResponse(output)
 
 def main_page(request):
-	# to load the main_page.html template
-	template = get_template('main_page.html')
-	# Set the variable in the template
-	variables = Context({
-		'head_title': 'Main page',
-		'page_title': 'Welcome to Elmusico',
-		'page_body': 'Blah blah blah blah blah'
-		})
-	#pass the variables and create a HTML output
-	output = template.render(variables)
-	#return it
-	return HttpResponse(output)
+	return render_to_response(
+		'main_page.html',
+		# {'user': request.user},
+		context_instance = RequestContext(request)
+	)
+
+def logout_page(request):
+	logout(request)
+	return HttpResponseRedirect('/')
 
 def user_page(request, username):
 	try:
@@ -33,10 +45,36 @@ def user_page(request, username):
 	ScoreSheetList = user.favorite_set.all()
 
 	template = get_template('user_page.html')
-	variables = Context({
+	variables = RequestContext(request, {
 		'username': username,
 		'favoriteList': ScoreSheetList
 		})
 
 	output = template.render(variables)
-	return HttpResponse(output)
+	return render_to_response('user_page.html', variables)
+
+def register_page(request):
+	print request
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			# Some magic SQL happen here!
+			
+			user = User.objects.create_user(
+				username=form.cleaned_data['username'],
+				# print username;
+				password=form.cleaned_data['password1'],
+				# print password;
+				# email=form.cleaned_data['email']
+				)
+			return HttpResponseRedirect('/register/success/')
+	else:
+		form = RegistrationForm()
+		variables = RequestContext(request, {
+			'form': form
+			})
+		return render_to_response(
+			'registration/register.html',
+			variables,
+			context_instance = RequestContext(request)
+			)
--- elmusico/urls.py
+++ elmusico/urls.py
@@ -1,9 +1,17 @@
+import os.path
+
+from django.views.generic import TemplateView, RedirectView
+
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
 from bookmarks.views import *
 admin.autodiscover()
+
+site_media = os.path.join(
+	os.path.dirname(__file__), '../site_media'
+)
 
 urlpatterns = patterns('',
     # Examples:
@@ -12,5 +20,11 @@ urlpatterns = patterns('',
     (r'^$', main_page),
     # \w: alphanumeric characters with underscore
     (r'^user/(\w+)/$', user_page),
+    (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', logout_page),
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': site_media }),
+    (r'^register/$', register_page),
+    (r'^register/success/$',
+        TemplateView.as_view(template_name='registration/register_success.html')),
     url(r'^admin/', include(admin.site.urls)),
 )
